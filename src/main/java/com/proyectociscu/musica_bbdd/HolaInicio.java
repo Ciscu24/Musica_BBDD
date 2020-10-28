49b569c501a451d33d683c1ce8e96e83efa0e1ed
matad <matad@LAPTOP-Eduardo>
2020-10-28 12:58:05 +0100

    Adios Prueba

--- src/main/java/com/proyectociscu/musica_bbdd/HolaInicio.java
+++ src/main/java/com/proyectociscu/musica_bbdd/HolaInicio.java
@@ -2,6 +2,6 @@ package com.proyectociscu.musica_bbdd;
 
 public class HolaInicio {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Adios");
     }
 }
