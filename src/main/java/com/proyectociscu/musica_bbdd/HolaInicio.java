f97503dae96fc0cfa25e9d1b2886c920e85b4d9e
Ciscu24 <ciscu6@gmail.com>
2020-10-29 11:27:39 +0100

    Commit02 Ciscu, Conexion a la base de datos lista ConnectionUtil y Connection a traves de fichero

--- src/main/java/com/proyectociscu/musica_bbdd/HolaInicio.java
+++ src/main/java/com/proyectociscu/musica_bbdd/HolaInicio.java
@@ -1,7 +1,9 @@
 package com.proyectociscu.musica_bbdd;
 
+import com.proyectociscu.musica_bbdd.utils.ConnectionUtil;
+
 public class HolaInicio {
     public static void main(String[] args) {
-        System.out.println("Adios");
+        System.out.println(ConnectionUtil.getConnection());
     }
 }
