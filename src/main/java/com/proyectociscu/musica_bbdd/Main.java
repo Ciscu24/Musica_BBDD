f7bc1206c5a09e8290a5e9ebb3c0e15224646782
matad <matad@DESKTOP-3HHADIB>
2020-11-02 16:44:35 +0100

    Commint 01 Eduardo | Cambio de package y GeneroDAO

--- src/main/java/com/proyectociscu/musica_bbdd/Main.java
+++ src/main/java/com/proyectociscu/musica_bbdd/Main.java
@@ -1,7 +1,12 @@
 package com.proyectociscu.musica_bbdd;
 
+import com.proyectociscu.musica_bbdd.model.Genero;
+import com.proyectociscu.musica_bbdd.model.GeneroDAO;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {             
+        //GeneroDAO hipohop=new GeneroDAO(4,"HOP");
+        // hipohop.save();
+        //System.out.println(RepositorioObjetos.generos);       
     }
 }
--- src/main/java/com/proyectociscu/musica_bbdd/RepositorioObjetos.java
+++ src/main/java/com/proyectociscu/musica_bbdd/RepositorioObjetos.java
@@ -1,18 +1,29 @@
 package com.proyectociscu.musica_bbdd;
 
+import com.proyectociscu.model.Lista;
 import com.proyectociscu.musica_bbdd.model.Artista;
 import com.proyectociscu.musica_bbdd.model.ArtistaDAO;
+import com.proyectociscu.musica_bbdd.model.Cancion;
+import com.proyectociscu.musica_bbdd.model.Comenta;
+import com.proyectociscu.musica_bbdd.model.Disco;
+import com.proyectociscu.musica_bbdd.model.Genero;
+import com.proyectociscu.musica_bbdd.model.GeneroDAO;
+import com.proyectociscu.musica_bbdd.model.Lista_Cancion;
+import com.proyectociscu.musica_bbdd.model.Reproduce;
+import com.proyectociscu.musica_bbdd.model.Subscripcion;
+import com.proyectociscu.musica_bbdd.model.Usuario;
 import java.util.List;
 
 public class RepositorioObjetos {
+
     public static List<Artista> artistas = ArtistaDAO.selectAll();
-    public static List<Cancion> = null;
-    public static List<Comenta> = null;
-    public static List<Disco> = null;
-    public static List<Genero> = null;
-    public static List<Lista> = null;
-    public static List<Lista_Cancion> = null;
-    public static List<Reproduce> = null;
-    public static List<Subscripcion> = null;
-    public static List<Ususario> = null;
+    public static List<Cancion> canciones = null;
+    public static List<Comenta> comentarios = null;
+    public static List<Disco> discos = null;
+    public static List<Genero> generos = GeneroDAO.selectAll();
+    public static List<Lista> listas = null;
+    public static List<Lista_Cancion> listas_canciones = null;
+    public static List<Reproduce> reproduciones = null;
+    public static List<Subscripcion> subcripciones = null;
+    public static List<Usuario> usuarios = null;
 }
